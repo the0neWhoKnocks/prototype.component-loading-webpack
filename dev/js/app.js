81f8d4b5eb2b031cfaa1e17b639ed6f2d7c4abcd
Nox <the0neWhoKnocks@users.noreply.github.com>
2016-03-27 14:15:14 -0700

    Updated config & structure
    * Got WebPack to bring in styles & templates when JS file was required.
    * Seperated out app & vendor code.
    * Deleted files & folders that didn't make sense anymore.
    * Removed files within public/js from VC since they're generated by WebPack.

---
+++ dev/js/app.js
@@ -0,0 +1,17 @@
+import $ from 'jquery';
+import './nike.js';
+
+// TODO - temp for testing
+import Chat from './components/Chat/Chat.js';
+
+// Anything that needs to occur after the page has loaded.
+$(function(){
+  var msg = "Libs have been loaded.";
+  
+  $('.js-msg').html(msg);
+  nike.log(msg);
+  
+  // TODO - temp for testing
+  var chat = new Chat();
+  $('body').append( chat.markup );
+});
