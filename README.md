2ee3e0dd060068d36065d5a4c8109ccaccf22569
Trevor Lemon <duhlemman@gmail.com>
2016-04-02 18:22:44 -0700

    Gulp & Uglify
    Added Gulp to the pipeline so that compiled files could be minified since you can't use the Uglify plugin in WebPack for files that are also in the CommonsChunkPlugin. Using Gulp has also fixed the Windows issue where extra Node processes were left running after you've exited a watcher.

--- README.md
+++ README.md
@@ -5,25 +5,10 @@
 ```
 npm i -dd -g webpack
 npm i -dd
-npm run wpBuild
+
+# You can then run this to build assets
+gulp webpack:build
+
+# Or this to build & start watching for changes.
+gulp webpack:watch
 ```
-
-## Dev
-
-Run the below command to watch for changes in the dev folder.
-
-```
-npm run wpWatch
-```
-
-Note - On Windows, if you run `wpWatch` then stop and start it. There will be a
-leftover `Node.js` process running. You'll start to see errors being thrown on
-save (even though the files are compiled), and you'll also see it Chunk the same
-file as many times for each rogue `Node.js` process. For now you have to kill the
-extra process via Task Manager after you stop the `wpWatch` command.
-
-## TO-DO
-
-* Currently, minified files don't work, but their full versions do. Getting this
-error when trying to load minified files: `Uncaught ReferenceError: webpackJsonp is not defined`.
-Look into UglifyJsPlugin opts to see if there's something for this.
--- dev/js/components/Chat/Chat.js
+++ dev/js/components/Chat/Chat.js
@@ -1,15 +1,40 @@
+import $ from 'jquery';
 import './Chat.styl';
 import template from './Chat.hbs';
 
 class Chat {
-  constructor(){
+  constructor(opts){
     this.namespace = 'chat';
-    this.jsPrefix = 'js-';
-    this.selectors = {};
-    
+    this.eventNamespace = '.'+this.namespace;
+    this.jsPrefix = '.js-'+this.namespace;
+    this.selectors = {
+      CTA: this.jsPrefix +'CTA'
+    };
+    this.events = {
+      CLICK: 'click'+ this.eventNamespace
+    };
     this.markup = template({
       namespace: this.namespace
     });
+    this.els = {
+      $container: undefined,
+      $cta: undefined
+    };
+    
+    var containerParent = (opts && opts.parent) ? opts.parent : 'body';
+    
+    this.els.$container = $(this.markup).appendTo(containerParent);
+    this.els.$cta = this.els.$container.find(this.selectors.CTA);
+    
+    this.addListeners();
+  }
+  
+  addListeners(){
+    this.els.$cta.on(this.events.CLICK, this.handleCtaClick.bind(this));
+  }
+  
+  handleCtaClick(ev){
+    alert('Start chat');
   }
 }
 
